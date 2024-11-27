aad112749d87cf2d9ecf8ff73f8204b416707904
jraporta <jraporta@gmail.com>
2024-11-27 10:19:44 +0100

    fist commit

---
+++ src/main/java/cat/jraporta/virtualpet/VirtualPetApplication.java
@@ -0,0 +1,13 @@
+package cat.jraporta.virtualpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/jraporta/virtualpet/VirtualPetApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.jraporta.virtualpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
